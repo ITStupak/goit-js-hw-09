5a649af04a9bfcf5a55b9bfd9ab412ddc420ebb1
Sergey Stupak <itstupak@gmail.com>
2024-05-27 13:13:31 +0200

    finished with styles

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,3 +2,12 @@ let formData = {
     email: "",
     message: "",
 };
+
+const labels = document.querySelectorAll('label');
+labels.forEach(label => { label.classList.add('form-label') });
+const input = document.querySelector('input');
+input.classList.add('form-input');
+const message = document.querySelector('textarea');
+message.classList.add('form-message');
+const button = document.querySelector('button');
+button.classList.add('form-btn');
