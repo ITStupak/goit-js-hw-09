02e299aa67dfb0cb408c41c2809b50624ba82bd9
ITStupak <itstupak@gmail.com>
2024-05-27 11:13:45 +0000

    Deploying to gh-pages from @ ITStupak/goit-js-hw-09@5a649af04a9bfcf5a55b9bfd9ab412ddc420ebb1 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e=document.querySelectorAll("label");e.forEach(t=>{t.classList.add("form-label")});const o=document.querySelector("input");o.classList.add("form-input");const s=document.querySelector("textarea");s.classList.add("form-message");const c=document.querySelector("button");c.classList.add("form-btn");
+//# sourceMappingURL=commonHelpers2.js.map
